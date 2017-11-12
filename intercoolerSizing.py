f20a656bedcc989a9a7a4e39f60471c909f0d76e
bcusick <bcusick@users.noreply.github.com>
2017-11-11 16:29:30 -0800

    stuff

--- intercoolerSizing.py
+++ intercoolerSizing.py
@@ -6,12 +6,12 @@ x = 1.0
 y = 1.0
 z = 1.0
 
-for x in range(1, 9):
+for x in range(1, 12):
     x+=1
-    for y in range(1,9):
+    for y in range(1,12):
         y+=1
-        for z in range(1,9):
-
+        for z in range(1,12):
+            z+=1
             ## initial variables and all dims in meters, frozen boost int000333
             coreHeight = x * 25.4/1000 #all dims in meters
             coreWidth = y * 25.4/1000
@@ -122,11 +122,9 @@ for x in range(1, 9):
             Tout = NTU['Tho']
             totalDim = x + y + z
             output = [Tout, x, y, z, totalDim]
-            if output[4]<21:
-                if output[0]<95:
+            #print output
+            if output[4]<25:
+                if output[0]<85:
                     print output
-            #print "Support power: "
-            #print Power
-
 
             ####testing
